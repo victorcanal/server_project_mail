2fe094169fc4460524cc0887766b66bfaf5ed987
Victor CANAL <canal.victor@yahoo.fr>
2020-12-14 19:58:37 +0100

    update main

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse;
+
+def connect():
+    return
+
+def read():
+    return
+
+def send(file):
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MUA in Python')
+
+    parser.add_argument('--text', metavar='example.txt',
+                        help='path to the text to send', default="null")
+
+    #args = parser.parse_args()
+    #make_get_request(args.type, args.destination)
+    #get_response(args.destination)
