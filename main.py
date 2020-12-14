bd221b79565457390b93249285d262d2d50f0b97
biguest <rodrigue.d.f@free.fr>
2020-12-14 21:52:54 +0100

    Corrections in the menu and removed ;

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sqlite3;
+import sqlite3
+
 
 def connect():
     conn = sqlite3.connect('mail.db')
@@ -9,60 +10,62 @@ def connect():
     conn.commit()
     conn.close()
 
+
 def read():
     conn = sqlite3.connect('mail.db')
     c = conn.cursor()
     for row in c.execute('SELECT * FROM mail ORDER BY source'):
         print(row)
-    conn.close();
+    conn.close()
+
 
 def send(file):
     return
-	
+
 
 def function_one():
     return " --- Executing function_one"
-	
+
 
 def function_two():
     return " --- Executing function_two"
-	
+
 
 def function_three():
     return " --- Executing function_three"
-	
 
-def switcher(case: int):
-    switcher = {
+
+def menu(case: int):
+    switch = {
         1: function_one,
         2: function_two,
         3: function_three
     }
-    func = switcher.get(case, lambda: "Switcher error")
+    func = switch.get(case, lambda: "Switcher error")
     func()
-	
+
 
 if __name__ == '__main__':
-	print("Menu :");
-	entry_names = { "entry 1", "entry 2", "entry 3" };
+    print("Menu :")
+    entry_names = ["entry 1", "entry 2", "entry 3"]
 
-	for i in range(len(entry_names)):
-		print("Entry " + str(i + 1) + " : " + entry_names[i]);
+    for i in range(len(entry_names)):
+        print("Entry " + str(i + 1) + " : " + entry_names[i])
 
-	entry_number = -1;
-	while True:
-		try:
-			print("Numéro de l'entrée désirée : ");
-			entry_number = int(input());
-		except ValueError:
-			print("Erreur : La réponse donnée n'est pas un nombre.");
-			break;
-		if entry_number > len(entry_names) or entry_number <= 0:
-			print("Erreur : La réponse donnée n'est pas dans la liste.");
-			break;
+    entry_number = -1
+    while True:
+        try:
+            print("Desired menu entry number: ")
+            entry_number = int(input())
+        except ValueError:
+            print("Error : The given answer is not a number.")
+            break
+        if entry_number > len(entry_names) or entry_number <= 0:
+            print("Error : The given answer is not in the list.")
+            break
 
-	print("Vous avez sélectionné l'exo " + str(entry_number) + " : " + str(entry_names[entry_number - 1]));
-	switcher(entry_number)
-	
-	#connect()
+    print("You selected menu entry number " + str(entry_number) + " : " + str(entry_names[entry_number - 1]))
+    menu(entry_number)
+
+    # connect()
     read()
